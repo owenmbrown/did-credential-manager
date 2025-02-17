50b71da82a9fec54e3ade436c16a4c377e9904ed
Nathan-Andrews <andrewsnathan2003@gmail.com>
2025-02-17 17:42:20 -0600

    handle VCs

--- wallet-setup/src/App.js
+++ wallet-setup/src/App.js
@@ -1,7 +1,12 @@
 import { useState } from "react";
 
+// https://docs.tuum.tech/identify
+
 function App() {
     const [account, setAccount] = useState("");
+    const [dateOfBirth,setDateOfBirth] = useState("");
+
+    const snapId = `npm:@hashgraph/hedera-identify-snap`
 
     async function getAccount() {
         try {
@@ -16,10 +21,119 @@ function App() {
         }
     }
 
+    async function isMetamaskInstalled() {
+        return typeof window.ethereum !== "undefined" && window.ethereum.isMetaMask;
+    }
+
+    async function connect() {
+        console.log("snap id", snapId);
+        const isMetamaskDetected = await isMetamaskInstalled();
+        if (!isMetamaskDetected ) {
+          console.error(
+            "You need to install Metamask for this demo to work. You can install it at https://metamask.io"
+          );
+          alert(
+            "You need to install Metamask for this demo to work. You can install it at https://metamask.io"
+          );
+        }
+      
+        let snaps = await window.ethereum.request({
+          method: "wallet_getSnaps"
+        });
+        console.log("Installed snaps...", snaps);
+        try {
+          if (!(snapId in snaps)) {
+            console.log("Identify Snap is not yet installed. Installing now...");
+            const result = await window.ethereum.request({
+              method: "wallet_requestSnaps",
+              params: {
+                [snapId]: {}
+              }
+            });
+            console.log("result: ", result);
+            snaps = await window.ethereum.request({
+              method: "wallet_getSnaps"
+            });
+          }
+        } catch (e) {
+          console.log(
+            `Failed to obtain installed snap: ${JSON.stringify(e, null, 4)}`
+          );
+          alert(`Failed to obtain installed snap: ${JSON.stringify(e, null, 4)}`);
+        }
+      
+        if (snapId in snaps) {
+          console.log("Connected successfully!");
+          alert("Connected successfully!");
+        } else {
+          console.log("Could not connect successfully. Please try again!");
+          alert("Could not connect successfully. Please try again!");
+        }
+    }
+      
+
+    const handleHelloAPIRequest = async () => {
+        await window.ethereum.request({
+            method: 'wallet_invokeSnap',
+            params: {
+                snapId,
+                request: {
+                    method: 'hello',
+                    params: {}
+                }
+            }
+        })
+    }
+
+    const handleSendHelloClick = async () => {
+        try {
+            await handleHelloAPIRequest();
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    const handleCreateVC = async () => {
+        const options = {
+            store: ['snap']
+        }
+
+        const vcValue = {
+            dateOfBirth: dateOfBirth
+        }
+
+        const metamaskAddress = account
+        
+        const params = {
+            metamaskAddress,
+            vcKey: 'profile',
+            vcValue: vcValue,
+            credTypes: ['DOBCredential'],
+            options
+        }
+
+        await window.ethereum.request({
+            method: 'wallet_invokeSnap',
+            params: {
+            snapId,
+            request: {
+                method: 'createVC',
+                params: params
+            }
+            }
+        })
+    }
+
     return (
         <div>
             <button onClick={getAccount}>Enable Ethereum</button>
             <h2>Account: <span>{account}</span></h2>
+            <button onClick={connect}>Connect</button>
+            <button onClick={handleSendHelloClick}>Hello test identify snap</button>
+
+            <p>Enter your date of birth: </p>
+            <input type="text" placeholder="Your Date of Birth" onChange={e => {setDateOfBirth(e.target.value)}}/>
+            <button onClick={() => handleCreateVC()}>Create Verifiable Credential</button>
         </div>
     );
 }
