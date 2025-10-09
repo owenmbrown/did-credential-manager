d37de31ab998b437533601290eac5310ef25d96e
Owen Brown <owenbrown926@gmail.com>
2025-10-09 16:28:11 -0500

    more cleanup

--- snap/packages/snap/src/components.tsx
+++
@@ -1,6 +0,0 @@
-import { CredentialCard } from './components/CredentialCard';
-import { DID } from './components/DID'
-import { InclusiveRow } from './components/InclusiveRow'
-import { TripleRow } from './components/TripleRow';
-
-export { DID, InclusiveRow, CredentialCard, TripleRow };
