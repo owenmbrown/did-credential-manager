68c794163bb793f2da832b2da2638ea51cce9d14
Owen Brown <owenbrown926@gmail.com>
2025-10-21 14:00:18 -0500

    fix npm hoist errors

--- demo/holder-wallet/vite.config.ts
+++ demo/holder-wallet/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,5 +13,12 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: true,
+  },
+  resolve: {
+    dedupe: ['react', 'react-dom'],
+    alias: {
+      react: path.resolve('./node_modules/react'),
+      'react-dom': path.resolve('./node_modules/react-dom'),
+    },
   },
 })
